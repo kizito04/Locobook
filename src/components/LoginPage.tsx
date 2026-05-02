d9f9b1ca862e1df3d415cae7afe9a641ab51d78f
Ahaisibwe Kizito <kizitoahaisibwe04@gmail.com>
2026-05-02 03:18:59 +0300

    mt

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -32,7 +32,7 @@ export const LoginPage: React.FC<LoginPageProps> = ({ onLogin, onCreateAccount }
             <img src={locologo} alt="Locobook logo" className="h-56 w-56 max-w-none object-contain"
           style={{ transform: 'translateY(-10px)' }} />
           </div>
-          <h1 className="relative z-10 -mt-6 text-3xl font-bold text-slate-900">Welcome Back</h1>
+          <h1 className="relative z-10 -mt-10 text-3xl font-bold text-slate-900">Welcome Back</h1>
         </div>
 
         <button
