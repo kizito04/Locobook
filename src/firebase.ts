1611558f4abdbb99e2e248f14ad734b79d5b9061
Ahaisibwe Kizito <kizitoahaisibwe04@gmail.com>
2026-05-08 22:02:04 +0300

    auth

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,7 +3,10 @@ import { getAuth } from 'firebase/auth';
 import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+  ...firebaseConfig,
+  authDomain: 'locobook.vercel.app'
+});
 export const auth = getAuth(app);
 
 // Use initializeFirestore with long polling to improve connectivity in restricted environments
